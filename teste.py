025c62c581f397285c22bedce54cd49fdd251f24
joao lucas <cavalcanti.joao@academico.ifpb.edu.br>
2025-06-02 14:42:05 -0300

    alterado requirements-colab.txt

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 
 tpu = tf.distribute.cluster_resolver.TPUClusterResolver("local")
-print("Rodando na tpu", tpu.master())
+print("Rodando na tpu: ", tpu.cluster_spec().as_dict())
