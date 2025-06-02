17bd11cbd3997f03d6ff71749f237940812d78c4
joao lucas <cavalcanti.joao@academico.ifpb.edu.br>
2025-06-02 15:08:47 -0300

    removido o "local" dos tpuclusterresolver

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 
-tpu = tf.distribute.cluster_resolver.TPUClusterResolver("local")
+tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
 print("Rodando na tpu: ", tpu.cluster_spec().as_dict())
